2445526ce3adf1eeb2c486c150429b2df58150d8
monkeydgarpoden <115400588+monkeydgarpoden@users.noreply.github.com>
2022-10-13 15:14:15 +0530

    bug fix useeffect

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,6 +19,7 @@ export const Navbar = () => {
 
     useEffect(() => {
         showButton()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
     window.addEventListener('resize', showButton)
